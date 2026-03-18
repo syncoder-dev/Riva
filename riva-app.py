9a215b7490965afaf8ecb9ff4d160e86f07c4f8e
syncoder-dev <praagya.alt@gmail.com>
2026-03-18 03:08:32 -0700

    Update riva-app.py

--- riva-app.py
+++ riva-app.py
@@ -27,7 +27,7 @@ if user_input:
 
     try:
         response = client.chat.completions.create(
-            model="mixtral-8x7b-32768",
+            model="llama-8b-8192",
             messages=st.session_state.messages
         )
 
